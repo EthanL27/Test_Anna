af5c5ea75ce2368624d52147bd2923cf026a45bd
Ethan <ethan.lalle@hotmail.fr>
2026-03-31 16:32:02 +0200

    Correction du bug JS par Bob

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var date = new Date(); var h = date.getHours(); var m = date.getMinutes(); var s
 h = (h < 10) ? "0" + h : h;
 m = (m < 10) ? "0" + m : m;
 s = (s < 10) ? "0" + s : s;
-var time = h + ":" + m + ":" + s + " " + session; document.getElementById("DigitalCLOCK").innerText = time; document.getElementById("DigitalCLOCK").textContent = time;
+var time = h + ":" + m + ":" + s; document.getElementById("DigitalCLOCK").innerText = time; document.getElementById("DigitalCLOCK").textContent = time;
 setTimeout(showTime, 1000);
 showTime();
